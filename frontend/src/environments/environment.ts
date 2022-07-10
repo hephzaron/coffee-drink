1c9ebcab76e6a02f487aa0e69eda617ea152fcab
Daramola Tobi <tobi_daramola@yahoo.com>
2022-07-10 18:19:42 +0100

    setup application and linting style

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
 /* @TODO replace with your variables
  * ensure all variables on this page match your project
  */
+const dotEnv = require('dotenv');
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
+    url: process.env.AUTH0_DOMAIN_PREFIX, // the auth0 domain prefix
     audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    clientId: process.env.AUTH0_CLIENT_ID, // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
