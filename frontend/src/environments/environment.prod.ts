d6b5e5a0f74829dd7c5b25e4c0b44cb447b4819c
Daramola Tobi <tobi_daramola@yahoo.com>
2022-07-14 13:15:43 +0100

    chore: configure deployment to heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'hephzy-coffee.us', // the auth0 domain prefix
+    audience: 'drink', // the audience set for the auth0 app
+    clientId: 'LA14RRAyNWjPGAqs1UpRTj7lcUhZ93qp', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+  }
 };
